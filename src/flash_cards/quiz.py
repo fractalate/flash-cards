d04c7146a210d8c7aff0ec86030bfecfb549568e
Kate Novak <kje@cochleoid.com>
2026-02-13 22:29:15 -0500

    Something Like A Rating System

--- src/flash_cards/quiz.py
+++ src/flash_cards/quiz.py
@@ -1,5 +1,6 @@
 import os
 
+from sqlalchemy import func, case, literal, or_
 from sqlalchemy.orm import Session
 
 from flash_cards.database import (
@@ -22,10 +23,51 @@ def ensure_database() -> None:
 
 
 def get_next_card(session: Session) -> None | Card:
-    card = (
+    adjusted_ratings_cte = (
+        session.query(
+            Rating,
+            func.avg(Rating.rating).over(
+                partition_by=Rating.card_id,
+                order_by=Rating.created_at.desc(),
+                rows=(-5, 0),
+            ).label("average_rating"),
+            func.row_number().over(
+                partition_by=Rating.card_id,
+                order_by=Rating.created_at.desc(),
+            ).label("card_rating_number"),
+            func.datetime(
+                Rating.created_at,
+                case(
+                    (Rating.rating == literal(1), literal("+0 minutes")),
+                    (Rating.rating == literal(2), literal("+5 minutes")),
+                    (Rating.rating == literal(3), literal("+30 minutes")),
+                    (Rating.rating == literal(4), literal("+24 hours")),
+                    else_=literal("+0 minutes"),
+                ),
+            ).label("due_at"),
+        )
+    ).cte("adjusted_ratings_cte")
+
+    card_query = (
         session.query(Card)
-        .limit(1)
-    ).first()
+        .outerjoin(
+            adjusted_ratings_cte,
+            Card.card_id == adjusted_ratings_cte.c.card_id,
+        )
+        .where(
+            or_(
+                adjusted_ratings_cte.c.card_rating_number.is_(None),
+                adjusted_ratings_cte.c.card_rating_number == literal(1),
+            )
+        )
+        .order_by(
+            adjusted_ratings_cte.c.due_at.asc().nulls_first(),
+            adjusted_ratings_cte.c.average_rating,
+        )
+    )
+
+    card = card_query.first()
+
     return card
 
 
@@ -46,7 +88,7 @@ def store_rating(session: Session, card: Card, rating: int) -> None:
     session.commit()
 
 
-def main(argv: list[str]):
+def main(argv: list[str]) -> None:
     ensure_database()
     Session = get_session_maker(DATABASE_URL)
 
@@ -58,12 +100,19 @@ def main(argv: list[str]):
                 print("no more cards")
                 break
 
-            input(card.prompt + " [press return to reveal]")
+            print()
+            print(card.prompt)
+            print()
+            input("[press return to reveal]")
+            print()
             print(card.answer)
+            print()
 
             rating = collect_rating()
-
             store_rating(session, card, rating)
+
+            print()
+
 
 if __name__ == "__main__":
     import sys
